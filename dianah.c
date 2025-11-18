785f61f196bfe9123e83f3338513636ef95de4b7
maur-ice <mauricegalen7@gmail.com>
2025-11-18 14:11:22 +0300

    added hello world by group F

---
+++ dianah.c
@@ -0,0 +1,15 @@
+// LUBWAMA MAURICE
+// AMPURIRA PHILBERT
+// NABUTOONO DIANAH NDOMBI
+// MPUUGA JONATHAN
+// LUBOWA EMMANUEL
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("HELLO WORLD\n");
+
+    return 0;
+}
